3ade4078efc847ac4ef55f8ba1501e3d890f0f04
nxfive <kontakt@nxfive.pl>
2025-10-27 23:28:58 +0100

    feat: Create OptunaConfig class, add type annotations

--- src/models/settings.py
+++ src/models/settings.py
@@ -1,24 +1,25 @@
 import yaml
+from typing import Any
 
 from src.utils.paths import MODEL_CONFIG
 
 
 class FeaturesConfig:
-    def __init__(self, config):
+    def __init__(self, config: dict[str, Any]):
         self.numeric = config.get("num_features", [])
         self.categorical = config.get("cat_features", [])
         self.binary = config.get("bin_features", [])
 
 
 class ModelConfig:
-    def __init__(self, config):
+    def __init__(self, config: dict[str, Any]):
         self.preprocess_num_features = config.get("preprocess_num_features", True)
         self.target_transformation = config.get("target_transformations", False)
         self.params = config.get("params", {})
 
 
 class PipelineConfig:
-    def __init__(self, yaml_path):
+    def __init__(self, yaml_path: str):
         with open(yaml_path) as f:
             cfg = yaml.safe_load(f)
 
@@ -35,6 +36,16 @@ class PipelineConfig:
         self.cv = cfg.get("cv", {})
         self.scoring = cfg.get("scoring", [])
         self.transformations = cfg.get("transformations", {})
+
+
+class OptunaConfig:
+    def __init__(self, yaml_path: str, model: type):
+        with open(yaml_path) as f:
+            cfg = yaml.safe_load(f)
+        
+        self.model = model
+        self.params = cfg.get(model.__name__, None)
+        self.trials = cfg["optuna"].get("trials", 50)
 
 
 pipeline_config = PipelineConfig(MODEL_CONFIG)
