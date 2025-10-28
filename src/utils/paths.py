8016e435757df42a0ba9d8c9c248ec73cdd8c295
nxfive <kontakt@nxfive.pl>
2025-10-28 18:24:21 +0100

    feat: Add path to models/ dir and optuna config file

--- src/utils/paths.py
+++ src/utils/paths.py
@@ -3,6 +3,10 @@ import os
 
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 
-MODEL_CONFIG = os.path.join(PROJECT_ROOT, "src", "models", "config.yml")
+MODEL_CONFIG = os.path.join(PROJECT_ROOT, "config", "model_config.yml")
+OPTUNA_CONFIG = os.path.join(PROJECT_ROOT, "config", "optuna.yml")
+
 RAW_DATA_DIR = os.path.join(PROJECT_ROOT, "data", "raw")
 PROCESSED_DATA_DIR = os.path.join(PROJECT_ROOT, "data", "processed")
+
+MODELS_DIR = os.path.join(PROJECT_ROOT, "models")
