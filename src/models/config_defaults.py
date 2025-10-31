2c4579c31496fd5b9bc06136846e4e538bc70e5e
nxfive <kontakt@nxfive.pl>
2025-10-31 21:39:13 +0100

    feat: Add default optuna config

--- src/models/config_defaults.py
+++ src/models/config_defaults.py
@@ -31,3 +31,45 @@ MODEL_DEFAULT_CONFIG = {
         "none": {"params": {}},
     },
 }
+
+OPTUNA_DEFAULT_CONFIG = {
+    "optuna": {"trials": 10},
+    "models": {
+        "RandomForestRegressor": {
+            "n_estimators": {"min": 50, "max": 100, "step": 10},
+            "max_depth": {
+                "min": 2,
+                "max": 10,
+            },
+            "min_samples_split": {
+                "min": 3,
+                "max": 7,
+            },
+            "min_samples_leaf": {
+                "min": 3,
+                "max": 7,
+            },
+        },
+        "DecisionTreeRegressor": {
+            "max_depth": {
+                "min": 2,
+                "max": 10,
+            },
+            "min_samples_split": {
+                "min": 3,
+                "max": 7,
+            },
+            "min_samples_leaf": {"min": 3, "max": 7},
+        },
+        "KNeighborsRegressor": {
+            "n_neighbors": {
+                "min": 3,
+                "max": 7,
+            },
+            "leaf_size": {
+                "min": 10,
+                "max": 20,
+            },
+        },
+    },
+}
