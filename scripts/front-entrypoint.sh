a245a2d295d0bcbce58994bf08e21f9efd1c65b4
nxfive <kontakt@nxfive.pl>
2025-11-08 21:15:10 +0100

    chore: Create entrypoint for frontend service

---
+++ scripts/front-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+echo "Waiting for server..."
+
+while ! curl -s http://$BENTO_HOST:$BENTO_PORT > /dev/null; do
+  sleep 2
+done
+
+echo "Server ready"
+
+cd client
+streamlit run app.py --server.port 8501 --server.address 0.0.0.0
