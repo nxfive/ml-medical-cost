a970a6f286b2daefe758e69023095ad3397a0f73
nxfive <kontakt@nxfive.pl>
2025-11-08 18:41:57 +0100

    feat: Add method to save prediction to database

--- services/backend/service.py
+++ services/backend/service.py
@@ -1,9 +1,12 @@
 import bentoml
 import pandas as pd
 from pydantic import BaseModel
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import Session
 
-with bentoml.importing():
-    from src.features.features import convert_features_type
+from database.db import get_db
+from database.models import MedicalPrediction
+from src.features.features import convert_features_type
 
 
 class MedicalCostFeatures(BaseModel):
@@ -11,7 +14,7 @@ class MedicalCostFeatures(BaseModel):
     sex: str
     bmi: float
     children: int
-    smoker: str 
+    smoker: str
     region: str
 
 
@@ -21,21 +24,52 @@ class MedicalRegressorService:
     def __init__(self):
         self.model = bentoml.models.get("medical_regressor:latest").load_model()
 
+    def save_prediction_to_db(
+        self, db: Session, prediction, features: MedicalCostFeatures
+    ):
+        new_prediction = MedicalPrediction(
+            age=features.age,
+            sex=features.sex,
+            bmi=features.bmi,
+            children=features.children,
+            smoker=features.smoker,
+            region=features.region,
+            predicted_charge=float(prediction),
+        )
+        try:
+            db.add(new_prediction)
+            db.commit()
+            db.refresh(new_prediction)
+            return new_prediction
+        except SQLAlchemyError as e:
+            db.rollback()
+            print(f"Error saving prediction: {e}")
+            return None
+
     @bentoml.api()
     def predict(self, medical_features: MedicalCostFeatures):
         medical_features_df = pd.DataFrame([medical_features.model_dump()])
-        
         medical_features_df = convert_features_type(medical_features_df)
-        
-        prediction = self.model.predict(medical_features_df)
-        return {"charges": prediction[0]}
+
+        prediction = self.model.predict(medical_features_df)[0]
+
+        db = next(get_db())
+        self.save_prediction_to_db(db, prediction, medical_features)
+
+        return {"charges": prediction}
 
     @bentoml.api()
     def predict_multiple(self, medical_features_list: list[MedicalCostFeatures]):
-        medical_features_dicts = [features.model_dump() for features in medical_features_list]
+        medical_features_dicts = [
+            features.model_dump() for features in medical_features_list
+        ]
         medical_features_df = pd.DataFrame(medical_features_dicts)
-        
         medical_features_df = convert_features_type(medical_features_df)
-        
+
         predictions = self.model.predict(medical_features_df)
+
+        db = next(get_db())
+        for prediction, features in zip(predictions, medical_features_list):
+            self.save_prediction_to_db(db, prediction, features)
+
         return {"charges": predictions.tolist()}
