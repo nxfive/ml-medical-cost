9811a524f5c0497a1cc3390f801621ad7c52a0b5
nxfive <kontakt@nxfive.pl>
2025-11-13 23:55:44 +0100

    chore: Add ssl mode, change to artifacts-destination, add localhost and docker service

--- scripts/mlflow-entrypoint.sh
+++ scripts/mlflow-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 mlflow server \
     --host 0.0.0.0 \
     --port 5000 \
-    --backend-store-uri postgresql://$POSTGRES_USER:$POSTGRES_PASS@$POSTGRES_SERVICE:$POSTGRES_PORT/$POSTGRES_DB \
-    --default-artifact-root /mlflow/artifacts \
+    --backend-store-uri "postgresql://$POSTGRES_USER:$POSTGRES_PASS@$POSTGRES_SERVICE:$POSTGRES_PORT/$POSTGRES_DB?sslmode=verify-full&sslrootcert=/certs/ca.crt&sslcert=/certs/client.crt&sslkey=/certs/client.key" \
+    --artifacts-destination ./mlflow_artifacts \
     --cors-allowed-origins "https://mlflow.nxfive.pl" \
-    --allowed-hosts $MLFLOW_HOST
+    --allowed-hosts "$MLFLOW_HOST,mlflow:5000,localhost:*,127.0.0.1:*"
