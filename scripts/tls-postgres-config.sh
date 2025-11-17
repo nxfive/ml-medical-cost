9db22e62baaeba3ef66458a99d0c41d949090d61
nxfive <kontakt@nxfive.pl>
2025-11-17 15:46:59 +0100

    chore: Create script to generate postgres config files

---
+++ scripts/tls-postgres-config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eu
+
+PATHS=("/config/backend" "/config/mlflow")
+
+for p in "${PATHS[@]}"; do      
+  echo "Create ssl/tls configuration file for postgres-${p#/config/}..."
+  echo "hostssl all all ${PG_SSL_ADDRESS} cert" > "${p}/pg_hba.conf"
+  
+  cat > "${p}/postgresql.conf" <<EOF
+listen_addresses = '*'
+port = 5432
+ssl = on
+ssl_cert_file = '${PG_MOUNT}/certs/server.crt'
+ssl_key_file = '${PG_MOUNT}/certs/server.key'
+ssl_ca_file = '${PG_MOUNT}/certs/ca.crt'
+EOF
+
+  echo "Done."
+
+done
