67ddc21f876e2447d10d370a91ac85f22418142a
nxfive <kontakt@nxfive.pl>
2025-11-18 19:40:19 +0100

    chore: Load environment from Docker secrets

--- scripts/back-entrypoint.sh
+++ scripts/back-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+export DATABASE_URL_PROD=$(cat /run/secrets/DATABASE_URL_PROD)
+export MLFLOW_TRACKING_URI=$(cat /run/secrets/MLFLOW_TRACKING_PROD)
+
+DB_HOST=$(cat /run/secrets/BACKEND_POSTGRES_HOST)
+DB_PORT=$(cat /run/secrets/POSTGRES_PORT)
+
 until nc -z $DB_HOST $DB_PORT; do
   echo "Waiting for database..."
   sleep 2
