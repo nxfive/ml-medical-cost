e048d66cb9b983d96bd05f6d212d72412898ff4d
nxfive <kontakt@nxfive.pl>
2025-11-23 14:28:02 +0100

    refactor: move BentoML startup to separate service

--- scripts/back-entrypoint.sh
+++ scripts/back-entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 export DATABASE_URL_PROD=$(cat /run/secrets/DATABASE_URL_PROD)
 export MLFLOW_TRACKING_URI=$(cat /run/secrets/MLFLOW_TRACKING_PROD)
+export BENTO_PORT=$(cat /run/secrets/BENTO_PORT)
 
 DB_HOST=$(cat /run/secrets/BACKEND_POSTGRES_HOST)
 DB_PORT=$(cat /run/secrets/POSTGRES_PORT)
@@ -15,13 +16,4 @@ done
 echo "Create DB migrations..."
 alembic upgrade head
 
-echo "Build BentoML service..."
-cd server
-bentoml build
-
-cd ..
-echo "Register model from MLflow"
-python -m server.bento
-
-echo "Start BentoML service..."
-python -m bentoml serve server.service:MedicalRegressorService
+uvicorn server.main:app --host 0.0.0.0 --port 8000
