fa52680f4dce8e2826257dfbe12884097dd356ef
nxfive <kontakt@nxfive.pl>
2025-11-23 12:51:55 +0100

    refactor: Move DB operations out of BentoML service

--- services/backend/service.py
+++ services/backend/service.py
@@ -1,22 +1,9 @@
 import bentoml
 import pandas as pd
-from pydantic import BaseModel
-from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy.orm import Session
 
 with bentoml.importing():
-    from database.db import get_db
-    from database.models import MedicalPrediction
     from src.features.features import convert_features_type
-
-
-class MedicalCostFeatures(BaseModel):
-    age: int
-    sex: str
-    bmi: float
-    children: int
-    smoker: str
-    region: str
+    from .schemas import MedicalCostFeatures
 
 
 @bentoml.service(name="medical_regressor_service")
@@ -25,37 +12,12 @@ class MedicalRegressorService:
     def __init__(self):
         self.model = bentoml.models.get("medical_regressor:latest").load_model()
 
-    def save_prediction_to_db(
-        self, db: Session, prediction, features: MedicalCostFeatures
-    ):
-        new_prediction = MedicalPrediction(
-            age=features.age,
-            sex=features.sex,
-            bmi=features.bmi,
-            children=features.children,
-            smoker=features.smoker,
-            region=features.region,
-            predicted_charge=float(prediction),
-        )
-        try:
-            db.add(new_prediction)
-            db.commit()
-            db.refresh(new_prediction)
-            return new_prediction
-        except SQLAlchemyError as e:
-            db.rollback()
-            print(f"Error saving prediction: {e}")
-            return None
-
     @bentoml.api()
     def predict(self, medical_features: MedicalCostFeatures):
         medical_features_df = pd.DataFrame([medical_features.model_dump()])
         medical_features_df = convert_features_type(medical_features_df)
 
         prediction = self.model.predict(medical_features_df)[0]
-
-        db = next(get_db())
-        self.save_prediction_to_db(db, prediction, medical_features)
 
         return {"charges": prediction}
 
@@ -68,9 +30,5 @@ class MedicalRegressorService:
         medical_features_df = convert_features_type(medical_features_df)
 
         predictions = self.model.predict(medical_features_df)
-
-        db = next(get_db())
-        for prediction, features in zip(predictions, medical_features_list):
-            self.save_prediction_to_db(db, prediction, features)
 
         return {"charges": predictions.tolist()}
