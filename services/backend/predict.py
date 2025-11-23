298200498044ca5a9570b660d778fb20e6b51dc4
nxfive <kontakt@nxfive.pl>
2025-11-23 22:06:33 +0100

    feat: Move database session from constructor to method parameters

--- services/backend/predict.py
+++ services/backend/predict.py
@@ -9,11 +9,10 @@ from .db import Database
 
 class PredictService:
 
-    def __init__(self, bento_url: str, db: Session):
+    def __init__(self, bento_url: str):
         self.bento_url = bento_url
-        self.database = Database(db)
 
-    def predict(self, features: MedicalCostFeatures) -> float:
+    def predict(self, db: Session, features: MedicalCostFeatures) -> float:
         payload = features.model_dump()
         df = convert_features_type(pd.DataFrame([payload]))
         converted = df.to_dict(orient="records")[0]
@@ -24,10 +23,11 @@ class PredictService:
         response.raise_for_status()
         prediction = response.json()["charges"]
 
-        self.database.create_record(data=payload, predicted_charge=prediction)
+        database = Database(db)
+        database.create_record(data=payload, predicted_charge=prediction)
         return prediction
 
-    def predict_many(self, features_list: list[MedicalCostFeatures]) -> list[float]:
+    def predict_many(self, db: Session, features_list: list[MedicalCostFeatures]) -> list[float]:
         payload = [f.model_dump() for f in features_list]
         df = convert_features_type(pd.DataFrame(payload))
         converted = df.to_dict(orient="records")
@@ -39,6 +39,7 @@ class PredictService:
         )
         response.raise_for_status()
         predictions = response.json()["charges"]
-
-        self.database.create_records(data_list=payload, predictions=predictions)
+        
+        database = Database(db)
+        database.create_records(data_list=payload, predictions=predictions)
         return predictions
