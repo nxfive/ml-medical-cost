5fc387b5112c37ae2ed1e349eb3c30ae1cd9e09f
nxfive <kontakt@nxfive.pl>
2025-11-25 15:25:05 +0100

    feat: Set nullable=False for required columns in MedicalPrediction

--- database/models.py
+++ database/models.py
@@ -7,10 +7,10 @@ class MedicalPrediction(Base):
     __tablename__ = "medical_predictions"
 
     id = Column(Integer, primary_key=True, index=True)
-    age = Column(Integer)
-    sex = Column(String)
-    bmi = Column(Float)
-    children = Column(Integer)
-    smoker = Column(String)
-    region = Column(String)
-    predicted_charge = Column(Float)
+    age = Column(Integer, nullable=False)
+    sex = Column(String, nullable=False)
+    bmi = Column(Float, nullable=False)
+    children = Column(Integer, nullable=False)
+    smoker = Column(String, nullable=False)
+    region = Column(String, nullable=False)
+    predicted_charge = Column(Float, nullable=False)
