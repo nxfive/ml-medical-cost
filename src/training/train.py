0310a29086ebccd29e0a2e4e0f494bebaefec1bc
nxfive <kontakt@nxfive.pl>
2025-11-27 14:20:18 +0100

    refactor: Migrate from custom pipeline_config to Hydra/OmegaConf

--- src/training/train.py
+++ src/training/train.py
@@ -1,19 +1,15 @@
-import numpy as np
-import pandas as pd
 from typing import Iterator, Sequence
 
+import numpy as np
+import pandas as pd
+from omegaconf import DictConfig
 from sklearn.compose import TransformedTargetRegressor
 from sklearn.pipeline import Pipeline
 
 from src.models.models import create_model_pipeline
-from src.models.settings import pipeline_config
-from src.models.tuning import (
-    evaluate_target_transformers,
-    perform_cross_validation,
-    perform_grid_search,
-)
-from src.models.utils import prepare_grid
-
+from src.training.tuning import (evaluate_target_transformers,
+                                 perform_cross_validation, perform_grid_search)
+from src.utils.utils import prepare_grid
 
 PipelineResult = tuple[
     Pipeline | TransformedTargetRegressor,
@@ -24,36 +20,36 @@ PipelineResult = tuple[
 ]
 
 
-def run_pipeline(
-    model: type, X_train: pd.DataFrame, X_test: pd.DataFrame, y_train: pd.Series
+def run_training(
+    model: type, X_train: pd.DataFrame, X_test: pd.DataFrame, y_train: pd.Series, cfg: DictConfig
 ) -> Iterator[tuple[PipelineResult, dict[str, list], str | None]]:
     """
     Runs the model pipeline with optional target transformations and hyperparameter tuning
     (using cross-validation or grid search with cross-validation).
     """
-    inner_pipeline = create_model_pipeline(model)
-    param_grid = prepare_grid(model)
+    inner_pipeline = create_model_pipeline(cfg, model)
+    param_grid = prepare_grid(cfg)
 
     def fit_pipeline(
-        pipeline: Pipeline | TransformedTargetRegressor, param_grid: dict[str, list]
+        pipeline: Pipeline | TransformedTargetRegressor, param_grid: dict[str, list], cfg: DictConfig
     ) -> PipelineResult:
-        if pipeline_config.models[model.__name__].params:
+        if cfg.model.params:
             return perform_grid_search(
-                pipeline, param_grid, X_train=X_train, X_test=X_test, y_train=y_train
+                pipeline, param_grid, X_train=X_train, X_test=X_test, y_train=y_train, cfg=cfg
             )
         else:
             return perform_cross_validation(
-                pipeline, X_train=X_train, X_test=X_test, y_train=y_train
+                pipeline, X_train=X_train, X_test=X_test, y_train=y_train, cfg=cfg
             )
 
-    if pipeline_config.models[model.__name__].target_transformations:
+    if cfg.model.target_transformations:
         for (
             pipeline,
             local_param_grid,
             transformer_name,
-        ) in evaluate_target_transformers(inner_pipeline, param_grid):
-            results = fit_pipeline(pipeline, local_param_grid)
+        ) in evaluate_target_transformers(inner_pipeline, param_grid, cfg):
+            results = fit_pipeline(pipeline, local_param_grid, cfg)
             yield results, local_param_grid, transformer_name
     else:
-        results = fit_pipeline(inner_pipeline, param_grid)
+        results = fit_pipeline(inner_pipeline, param_grid, cfg)
         yield results, param_grid, None
