09f65c06e497d54ced063adbe901aa4fe7c8fabe
nxfive <kontakt@nxfive.pl>
2025-11-27 14:04:28 +0100

    feat: Create func to laod splitted data to separated stage

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -183,3 +183,15 @@ def save_model_with_metadata(
 
     with open(metadata_path / f"{file_name}.yml", "w") as f:
         yaml.safe_dump(metadata, f)
+
+
+def load_splitted_data(cfg) -> tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+    """
+    Load splitted data from data/processed directory.
+    """
+    X_train = pd.read_parquet(Path(cfg.data.processed_dir) / "X_train.parquet")
+    X_test = pd.read_parquet(Path(cfg.data.processed_dir) / "X_test.parquet")
+    y_train = pd.read_parquet(Path(cfg.data.processed_dir) / "y_train.parquet").squeeze()
+    y_test = pd.read_parquet(Path(cfg.data.processed_dir) / "y_test.parquet").squeeze()
+
+    return X_train, X_test, y_train, y_test
