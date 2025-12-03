98934bdc873ee85f6e9417dcd431e06ca61cb7a9
nxfive <kontakt@nxfive.pl>
2025-12-03 16:57:16 +0100

    refactor: Delegate object creation to DataFactory in run()

--- src/data/run.py
+++ src/data/run.py
@@ -1,18 +1,10 @@
-from pathlib import Path
-
 from omegaconf import DictConfig
 
-from .io import DataReaderWriter, DatasetDownloader
 from .pipeline import DataPipeline
 
 
-def run_pipeline(cfg: DictConfig):
-    downloader = DatasetDownloader()
-    io_rw = DataReaderWriter()
+def run(cfg: DictConfig):
     pipeline = DataPipeline(
-        downloader,
-        io_rw,
-        raw_dir=Path(cfg.data.raw_dir),
-        processed_dir=Path(cfg.data.processed_dir),
+        cfg.data.raw_dir, cfg.data.processed_dir, cfg.kaggle.handle, cfg.kaggle.filename
     )
-    pipeline()
+    pipeline.run()
