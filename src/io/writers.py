4b43ec83f58a25606734d6a15e4f355ca4683a11
nxfive <kontakt@nxfive.pl>
2025-12-04 00:01:05 +0100

    feat: Add YamlWriter and JoblibWriter with proper type annotations

--- src/io/writers.py
+++ src/io/writers.py
@@ -2,16 +2,40 @@ from abc import ABC, abstractmethod
 from pathlib import Path
 
 import pandas as pd
+import yaml
+import joblib
+from sklearn.base import BaseEstimator
+
+from typing import Generic, TypeVar
+
+T = TypeVar("T")
 
 
-class BaseWriter(ABC):
+class BaseWriter(ABC, Generic[T]):
     @abstractmethod
-    def write(self, df: pd.DataFrame, path: Path): ...
+    def write(self, data: T, path: Path) -> None: ...
 
 
-class ParquetWriter(BaseWriter):
-    def write(self, df: pd.DataFrame, path: Path, index: bool = False):
+class ParquetWriter(BaseWriter[pd.DataFrame]):
+    def write(self, data: pd.DataFrame, path: Path) -> None:
         """
         Writes a pandas DataFrame to a Parquet file at the given path.
         """
-        df.to_parquet(path, index=index)
+        data.to_parquet(path, index=False)
+
+
+class YamlWriter(BaseWriter[dict]):
+    def write(self, data: dict, path: Path) -> None:
+        """
+        Writes a Python dictionary to a YAML file at the given path.
+        """
+        with open(path, "w") as f:
+            yaml.safe_dump(data, f)
+
+
+class JoblibWriter(BaseWriter[BaseEstimator]):
+    def write(self, data: BaseEstimator, path: Path) -> None:
+        """
+        Writes a Python object to a joblib file at the given path.
+        """
+        joblib.dump(data, path)
