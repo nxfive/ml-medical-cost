98152e5baf265d524fd06cf868738c6b17d8fd74
nxfive <kontakt@nxfive.pl>
2025-12-03 17:16:28 +0100

    refactor: Wrap training functions into TrainModel class

--- src/training/train.py
+++ src/training/train.py
@@ -4,11 +4,8 @@ import numpy as np
 import pandas as pd
 from omegaconf import DictConfig
 from sklearn.base import BaseEstimator
-from sklearn.pipeline import Pipeline
 
-from src.models.models import create_model_pipeline
-from src.training.tuning import (evaluate_target_transformers,
-                                 perform_cross_validation, perform_grid_search)
+from src.models.pipeline import create_model_pipeline
 from src.utils.grid import prepare_grid
 
 PipelineResult = tuple[
@@ -20,69 +17,87 @@ PipelineResult = tuple[
 ]
 
 
-def fit_estimator(
-    estimator: BaseEstimator,
-    param_grid: dict[str, list],
-    X_train: pd.DataFrame,
-    X_test: pd.DataFrame,
-    y_train: pd.Series,
-    cfg: DictConfig,
-) -> PipelineResult:
-    """
-    Fit an estimator using either GridSearchCV or simple cross-validation.
-    """
-    if cfg.model.params:
-        return perform_grid_search(
-            estimator,
-            param_grid,
-            X_train=X_train,
-            X_test=X_test,
-            y_train=y_train,
-            cfg=cfg,
+class TrainModel:
+    def __init__(
+        self,
+        model: type[BaseEstimator],
+        cfg_model: DictConfig,
+        cfg_features: DictConfig,
+        cfg_cv: DictConfig,
+        cfg_transform: DictConfig,
+        grid_runner,
+        cross_runner,
+        target_transformer,
+    ):
+        self.model_class = model
+        self.cfg_cv = cfg_cv
+        self.cfg_features = cfg_features
+        self.cfg_model = cfg_model
+        self.cfg_transform = cfg_transform
+        self.pipeline = create_model_pipeline(
+            self.cfg_model.preprocess_num_features, self.cfg_features
         )
-    else:
-        return perform_cross_validation(
-            estimator, X_train=X_train, X_test=X_test, y_train=y_train, cfg=cfg
-        )
+        self.param_grid = prepare_grid(self.cfg_model.params)
 
+        self.grid_runner = grid_runner
+        self.cross_runner = cross_runner
+        self.target_runner = target_transformer
 
-def generate_estimators_with_transformers(
-    pipeline: Pipeline, param_grid: dict[str, list], cfg: DictConfig
-) -> Iterator[tuple[BaseEstimator, dict[str, list], str | None]]:
-    """
-    Generate estimators with optional target transformations and updated parameter grids.
-    """
-    if cfg.model.target_transformations:
+    def fit_estimator(
+        self,
+        estimator: BaseEstimator,
+        X_train: pd.DataFrame,
+        X_test: pd.DataFrame,
+        y_train: pd.Series,
+    ) -> PipelineResult:
+        """
+        Fits an estimator using either GridSearchCV or simple cross-validation.
+        """
+        if self.model_params:
+            return self.grid_runner.run(
+                pipeline=estimator,
+                X_train=X_train,
+                X_test=X_test,
+                y_train=y_train,
+            )
+        else:
+            return self.cross_runner.run(
+                pipeline=estimator,
+                X_train=X_train,
+                X_test=X_test,
+                y_train=y_train,
+            )
+
+    def transform_estimator(
+        self,
+    ) -> Iterator[tuple[BaseEstimator, dict[str, list], str | None]]:
+        """
+        Generates estimators with optional target transformations and updated parameter grids.
+        """
+        if self.model_target_transformations:
+            for (
+                estimator,
+                local_param_grid,
+                transformer_name,
+            ) in self.target_transformer.evaluate():
+                yield estimator, local_param_grid, transformer_name
+        else:
+            yield self.pipeline, self.param_grid, None
+
+    def run(
+        self,
+        X_train: pd.DataFrame,
+        X_test: pd.DataFrame,
+        y_train: pd.Series,
+    ) -> Iterator[tuple[PipelineResult, dict[str, list], str | None]]:
+        """
+        Runs training over all estimators (with optional target transformations)
+        and performs either grid search or cross-validation.
+        """
         for (
             estimator,
             local_param_grid,
             transformer_name,
-        ) in evaluate_target_transformers(pipeline, param_grid, cfg):
-            yield estimator, local_param_grid, transformer_name
-    else:
-        yield pipeline, param_grid, None
-
-
-def run_training(
-    model: type,
-    X_train: pd.DataFrame,
-    X_test: pd.DataFrame,
-    y_train: pd.Series,
-    cfg: DictConfig,
-) -> Iterator[tuple[PipelineResult, dict[str, list], str | None]]:
-    """
-    Run training over all estimators (with optional target transformations)
-    and perform either grid search or cross-validation.
-    """
-    pipeline = create_model_pipeline(cfg, model)
-    param_grid = prepare_grid(cfg)
-
-    for (
-        estimator,
-        local_param_grid,
-        transformer_name,
-    ) in generate_estimators_with_transformers(pipeline, param_grid, cfg):
-        results = fit_estimator(
-            estimator, local_param_grid, X_train, X_test, y_train, cfg
-        )
-        yield results, local_param_grid, transformer_name
+        ) in self.transform_estimator():
+            results = self.fit_estimator(estimator, X_train, X_test, y_train)
+            yield results, local_param_grid, transformer_name
