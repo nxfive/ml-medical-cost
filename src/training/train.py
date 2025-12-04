5c1b6b590179fb886fce3fe7cf45706bc0c974ca
nxfive <kontakt@nxfive.pl>
2025-12-04 16:22:51 +0100

    refactor: Use dataclasses for TrainModel methods outputs

--- src/training/train.py
+++ src/training/train.py
@@ -1,33 +1,28 @@
-from typing import Iterator, Sequence
+from typing import Generator
 
-import numpy as np
 import pandas as pd
-from omegaconf import DictConfig
 from sklearn.base import BaseEstimator
 
+from src.conf.schema import (CVConfig, FeaturesConfig, ModelConfig,
+                             TransformersConfig)
 from src.models.pipeline import create_model_pipeline
-from src.utils.grid import prepare_grid
+from src.utils.grid import ParamGrid
 
-PipelineResult = tuple[
-    BaseEstimator,
-    Sequence[float],
-    float,
-    np.ndarray,
-    np.ndarray,
-]
+from .tuning import CrossValidationRunner, GridSearchRunner, TargetTransformer
+from .types import EvaluationResult, RunnerResult, TrainResult
 
 
 class TrainModel:
     def __init__(
         self,
         model: type[BaseEstimator],
-        cfg_model: DictConfig,
-        cfg_features: DictConfig,
-        cfg_cv: DictConfig,
-        cfg_transform: DictConfig,
-        grid_runner,
-        cross_runner,
-        target_transformer,
+        cfg_model: ModelConfig,
+        cfg_features: FeaturesConfig,
+        cfg_cv: CVConfig,
+        cfg_transform: TransformersConfig,
+        grid_runner: GridSearchRunner,
+        cross_runner: CrossValidationRunner,
+        target_transformer: TargetTransformer,
     ):
         self.model_class = model
         self.cfg_cv = cfg_cv
@@ -35,13 +30,12 @@ class TrainModel:
         self.cfg_model = cfg_model
         self.cfg_transform = cfg_transform
         self.pipeline = create_model_pipeline(
-            self.cfg_model.preprocess_num_features, self.cfg_features
+            self.cfg_model.preprocess_num_features, self.cfg_features, model=self.model_class
         )
-        self.param_grid = prepare_grid(self.cfg_model.params)
-
+        self.param_grid = ParamGrid.create(self.cfg_model.params)
         self.grid_runner = grid_runner
         self.cross_runner = cross_runner
-        self.target_runner = target_transformer
+        self.target_transformer = target_transformer
 
     def fit_estimator(
         self,
@@ -49,11 +43,11 @@ class TrainModel:
         X_train: pd.DataFrame,
         X_test: pd.DataFrame,
         y_train: pd.Series,
-    ) -> PipelineResult:
+    ) -> RunnerResult:
         """
         Fits an estimator using either GridSearchCV or simple cross-validation.
         """
-        if self.model_params:
+        if self.cfg_model.params:
             return self.grid_runner.run(
                 pipeline=estimator,
                 X_train=X_train,
@@ -68,28 +62,34 @@ class TrainModel:
                 y_train=y_train,
             )
 
-    def transform_estimator(
-        self,
-    ) -> Iterator[tuple[BaseEstimator, dict[str, list], str | None]]:
+    def transform_estimator(self) -> Generator[EvaluationResult, None, None]:
         """
         Generates estimators with optional target transformations and updated parameter grids.
         """
-        if self.model_target_transformations:
+        if self.cfg_model.target_transformations:
             for (
                 estimator,
                 local_param_grid,
                 transformer_name,
             ) in self.target_transformer.evaluate():
-                yield estimator, local_param_grid, transformer_name
+                yield EvaluationResult(
+                    estimator=estimator,
+                    param_grid=local_param_grid,
+                    transformation_name=transformer_name,
+                )
         else:
-            yield self.pipeline, self.param_grid, None
+            yield EvaluationResult(
+                estimator=self.pipeline,
+                param_grid=self.param_grid,
+                transformation_name=None,
+            )
 
     def run(
         self,
         X_train: pd.DataFrame,
         X_test: pd.DataFrame,
         y_train: pd.Series,
-    ) -> Iterator[tuple[PipelineResult, dict[str, list], str | None]]:
+    ) -> Generator[TrainResult, None, None]:
         """
         Runs training over all estimators (with optional target transformations)
         and performs either grid search or cross-validation.
@@ -100,4 +100,8 @@ class TrainModel:
             transformer_name,
         ) in self.transform_estimator():
             results = self.fit_estimator(estimator, X_train, X_test, y_train)
-            yield results, local_param_grid, transformer_name
+            yield TrainResult(
+                runner_result=results,
+                param_grid=local_param_grid,
+                transformation_name=transformer_name,
+            )
