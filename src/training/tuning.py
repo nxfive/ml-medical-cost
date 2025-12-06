f6b85517874b7959269f89a6f8af05540d28addf
nxfive <kontakt@nxfive.pl>
2025-12-06 18:18:18 +0100

    refactor: Pass estimator/pipeline and param_grid to run/evaluate

--- src/training/tuning.py
+++ src/training/tuning.py
@@ -10,15 +10,15 @@ from sklearn.pipeline import FunctionTransformer, Pipeline
 from sklearn.preprocessing import QuantileTransformer
 
 from src.conf.schema import TransformersConfig
-from src.utils.grid import ParamGrid
+from src.evaluation.metrics import compute_scores_mean
+from src.params.grid import ParamGrid
 
-from .core import compute_scores_mean
 from .types import EvaluationResult, RunnerResult
 
 
 class BaseRunner(ABC):
     @abstractmethod
-    def run(self, *args, **kwargs):
+    def run(self, *args, **kwargs) -> RunnerResult:
         """
         Runs the training/evaluation. Must be implemented in subclasses.
         """
@@ -32,35 +32,41 @@ class BaseRunner(ABC):
 
 
 class CrossValidationRunner(BaseRunner):
-    def __init__(self, cv: KFold, pipeline: Pipeline):
+    def __init__(self, cv: KFold):
         self.cv = cv
-        self.pipeline = pipeline
 
     def perform_cross_validation(
-        self, X_train: pd.DataFrame, y_train: pd.Series
+        self, estimator: BaseEstimator, X_train: pd.DataFrame, y_train: pd.Series
     ) -> list[np.float64]:
         """
-        Performs cross-validation on the given pipeline using the configuration provided.
+        Performs cross-validation on the given estimator using the configuration provided.
         """
-        return cross_val_score(self.pipeline, X_train, y_train, self.cv, scoring="r2")
+        return cross_val_score(estimator, X_train, y_train, self.cv, scoring="r2")
 
-    def train_pipeline(self, X_train: pd.DataFrame, y_train: pd.Series) -> Pipeline:
+    @staticmethod
+    def fit_estimator(
+        estimator: BaseEstimator, X_train: pd.DataFrame, y_train: pd.Series
+    ) -> BaseEstimator:
         """
-        Fits the given pipeline on the training data.
+        Fits the given estimator on the training data.
         """
-        self.pipeline.fit(X_train, y_train)
-        return self.pipeline
+        estimator.fit(X_train, y_train)
+        return estimator
 
     def run(
-        self, X_train: pd.DataFrame, X_test: pd.DataFrame, y_train: pd.Series
+        self,
+        estimator: BaseEstimator,
+        X_train: pd.DataFrame,
+        X_test: pd.DataFrame,
+        y_train: pd.Series,
     ) -> RunnerResult:
         """
-        Performs cross-validation, fits the pipeline and generates predictions for train and
+        Performs cross-validation, fits the estimator and generates predictions for train and
         test sets.
         """
-        folds_scores = self.perform_cross_validation(self.pipeline, X_train, y_train)
+        folds_scores = self.perform_cross_validation(estimator, X_train, y_train)
         folds_scores_mean = compute_scores_mean(folds_scores)
-        trained = self.train_pipeline(self.pipeline, X_train, y_train)
+        trained = self.fit_estimator(estimator, X_train, y_train)
         train_predictions = self.make_predictions(trained, X_train)
         test_predictions = self.make_predictions(trained, X_test)
 
@@ -74,29 +80,29 @@ class CrossValidationRunner(BaseRunner):
 
 
 class GridSearchRunner(BaseRunner):
-    def __init__(self, cv: KFold, pipeline: Pipeline, param_grid: dict[str, list]):
+    def __init__(self, cv: KFold):
         self.cv = cv
-        self.pipeline = pipeline
-        self.param_grid = param_grid
 
-    def perform_grid_search(self) -> GridSearchCV:
+    def perform_grid_search(
+        self, estimator: BaseEstimator, param_grid: dict[str, list]
+    ) -> GridSearchCV:
         """
-        Creates a GridSearchCV object for the given pipeline and parameter grid.
+        Creates a GridSearchCV object for the given estimator and parameter grid.
         """
         return GridSearchCV(
-            self.pipeline,
-            self.param_grid,
-            self.cv,
+            estimator,
+            param_grid,
+            cv=self.cv,
             scoring="r2",
             return_train_score=True,
         )
 
     @staticmethod
-    def train_grid_search(
+    def fit_grid_search(
         grid: GridSearchCV, X_train: pd.DataFrame, y_train: pd.Series
     ) -> BaseEstimator:
         """
-        Fits GridSearchCV on the training data and returns the best estimator pipeline.
+        Fits GridSearchCV on the training data and returns the best estimator.
         """
         grid.fit(X_train, y_train)
         return grid.best_estimator_
@@ -114,14 +120,19 @@ class GridSearchRunner(BaseRunner):
         ]
 
     def run(
-        self, X_train: pd.DataFrame, X_test: pd.DataFrame, y_train: pd.Series
+        self,
+        estimator: BaseEstimator,
+        param_grid: dict[str, list],
+        X_train: pd.DataFrame,
+        X_test: pd.DataFrame,
+        y_train: pd.Series,
     ) -> RunnerResult:
         """
-        Performs a GridSearchCV on the given pipeline, fits the best estimator and
+        Performs a GridSearchCV on the given estimator, fits the best estimator and
         generates predictions on training and test sets.
         """
-        grid = self.perform_grid_search()
-        trained = self.train_grid_search(grid, X_train, y_train)
+        grid = self.perform_grid_search(estimator, param_grid)
+        trained = self.fit_grid_search(grid, X_train, y_train)
         train_predictions = self.make_predictions(trained, X_train)
         test_predictions = self.make_predictions(trained, X_test)
         folds_scores = self.get_grid_folds_scores(grid)
@@ -143,60 +154,53 @@ class TargetTransformer:
         "none": None,
     }
 
-    def __init__(
-        self,
-        pipeline: Pipeline,
-        param_grid: dict[str, list],
-        cfg_transform: TransformersConfig,
-    ):
-        self.pipeline = pipeline
-        self.param_grid = param_grid
+    def __init__(self, cfg_transform: TransformersConfig):
         self.cfg_transform = cfg_transform
 
     @classmethod
-    def get(cls, name) -> BaseEstimator | None:
+    def get(cls, name: str) -> BaseEstimator | None:
         """
         Maps a string name to a scikit-learn transformer.
         """
         return cls.TRANSFORMERS.get(name)
 
     def build_wrapper_pipeline(
-        self, transformer: BaseEstimator | None
+        self, pipeline: Pipeline, transformer: BaseEstimator | None
     ) -> BaseEstimator:
         """
         Wraps the pipeline in TransformedTargetRegressor if transformer is provided.
         """
         if transformer is None:
-            return self.pipeline
-        return TransformedTargetRegressor(
-            regressor=self.pipeline, transformer=transformer
-        )
+            return pipeline
+        return TransformedTargetRegressor(regressor=pipeline, transformer=transformer)
 
-    def prepare_param_grid(self, params: dict[str, list]) -> dict[str, list]:
+    def prepare_param_grid(
+        self, param_grid: dict[str, list], params: dict[str, list]
+    ) -> dict[str, list]:
         """
         Merges base param grid with transformer parameters.
         """
-        grid_copy = self.param_grid.copy() if self.param_grid else {}
+        grid_copy = param_grid.copy() if param_grid else {}
         grid_copy = ParamGrid.prefix(grid_copy, "regressor")
         if params:
             grid_copy.update(ParamGrid.prefix(params, "transformer"))
         return grid_copy
 
     def evaluate(
-        self,
+        self, pipeline: Pipeline, param_grid: dict[str, list]
     ) -> Generator[EvaluationResult, None, None]:
         """
         Generates pipelines with target transformations and updated param grid.
         """
         for transformation_name, value in self.cfg_transform.to_dict().items():
             transformer = TargetTransformer.get(transformation_name)
-            estimator = self.build_wrapper_pipeline(transformer)
+            estimator = self.build_wrapper_pipeline(pipeline, transformer)
 
             if transformer is not None:
-                params = {k: list(v) for k, v in value["params"].items()}
-                local_param_grid = self.prepare_param_grid(params)
+                params = value["params"]
+                local_param_grid = self.prepare_param_grid(param_grid, params)
             else:
-                local_param_grid = self.param_grid
+                local_param_grid = param_grid
 
             yield EvaluationResult(
                 estimator=estimator,
