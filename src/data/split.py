af44bda3d744630d3094aa51de0b0baeebe66b24
nxfive <kontakt@nxfive.pl>
2025-12-06 16:09:18 +0100

    refactor: Remove DataSplitter; keep only standalone split functions

--- src/data/split.py
+++ src/data/split.py
@@ -1,34 +1,32 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-from src.data.types import SplitData
+from src.data.types import SplitData, SplitDataDict
 
 
-class DataSplitter:
-    @staticmethod
-    def split_features_target(
-        df: pd.DataFrame, target_col: str
-    ) -> tuple[pd.DataFrame, pd.Series]:
-        """
-        Splits DataFrame into features X and target y.
-        """
-        X = df.drop([target_col], axis=1)
-        y = df[target_col]
-        return X, y
+def split_features_target(
+    df: pd.DataFrame, target_col: str
+) -> tuple[pd.DataFrame, pd.Series]:
+    """
+    Splits DataFrame into features X and target y.
+    """
+    X = df.drop([target_col], axis=1)
+    y = df[target_col]
+    return X, y
 
-    @staticmethod
-    def split_train_test(
-        X: pd.DataFrame, y: pd.Series, test_size=0.2, random_state=42
-    ) -> SplitData:
-        """
-        Splits features and target into train/test sets.
-        """
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=test_size, random_state=random_state
-        )
-        return SplitData(
-            X_train=X_train,
-            X_test=X_test,
-            y_train=y_train.to_frame(),
-            y_test=y_test.to_frame(),
-        )
+
+def split_train_test(
+    X: pd.DataFrame, y: pd.Series, test_size=0.2, random_state=42
+) -> SplitDataDict:
+    """
+    Splits features and target into train/test sets.
+    """
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_size, random_state=random_state
+    )
+    return SplitData(
+        X_train=X_train,
+        X_test=X_test,
+        y_train=y_train,
+        y_test=y_test,
+    ).to_dict()
