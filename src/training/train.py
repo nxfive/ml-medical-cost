a90af88181f01a5c49996a1c82cd673d2976b6ea
nxfive <kontakt@nxfive.pl>
2025-12-06 19:17:50 +0100

    refactor: Add pipeline, param_grid attributes to TrainModel constructor
    Inject pipeline and param_grid into TargetTransformer at runtime.

--- src/training/train.py
+++ src/training/train.py
@@ -2,11 +2,10 @@ from typing import Generator
 
 import pandas as pd
 from sklearn.base import BaseEstimator
+from sklearn.pipeline import Pipeline
 
 from src.conf.schema import (CVConfig, FeaturesConfig, ModelConfig,
                              TransformersConfig)
-from src.models.pipeline import create_model_pipeline
-from src.utils.grid import ParamGrid
 
 from .tuning import CrossValidationRunner, GridSearchRunner, TargetTransformer
 from .types import EvaluationResult, RunnerResult, TrainResult
@@ -20,6 +19,8 @@ class TrainModel:
         cfg_features: FeaturesConfig,
         cfg_cv: CVConfig,
         cfg_transform: TransformersConfig,
+        param_grid: dict[str, list],
+        pipeline: Pipeline,
         grid_runner: GridSearchRunner,
         cross_runner: CrossValidationRunner,
         target_transformer: TargetTransformer,
@@ -29,10 +30,8 @@ class TrainModel:
         self.cfg_features = cfg_features
         self.cfg_model = cfg_model
         self.cfg_transform = cfg_transform
-        self.pipeline = create_model_pipeline(
-            self.cfg_model.preprocess_num_features, self.cfg_features, model=self.model_class
-        )
-        self.param_grid = ParamGrid.create(self.cfg_model.params)
+        self.pipeline = pipeline
+        self.param_grid = param_grid
         self.grid_runner = grid_runner
         self.cross_runner = cross_runner
         self.target_transformer = target_transformer
@@ -40,6 +39,7 @@ class TrainModel:
     def fit_estimator(
         self,
         estimator: BaseEstimator,
+        param_grid: dict[str, list],
         X_train: pd.DataFrame,
         X_test: pd.DataFrame,
         y_train: pd.Series,
@@ -49,14 +49,15 @@ class TrainModel:
         """
         if self.cfg_model.params:
             return self.grid_runner.run(
-                pipeline=estimator,
+                estimator=estimator,
+                param_grid=param_grid,
                 X_train=X_train,
                 X_test=X_test,
                 y_train=y_train,
             )
         else:
             return self.cross_runner.run(
-                pipeline=estimator,
+                estimator=estimator,
                 X_train=X_train,
                 X_test=X_test,
                 y_train=y_train,
@@ -67,21 +68,11 @@ class TrainModel:
         Generates estimators with optional target transformations and updated parameter grids.
         """
         if self.cfg_model.target_transformations:
-            for (
-                estimator,
-                local_param_grid,
-                transformer_name,
-            ) in self.target_transformer.evaluate():
-                yield EvaluationResult(
-                    estimator=estimator,
-                    param_grid=local_param_grid,
-                    transformation_name=transformer_name,
-                )
+            yield from self.target_transformer.evaluate(self.pipeline, self.param_grid)
         else:
             yield EvaluationResult(
                 estimator=self.pipeline,
                 param_grid=self.param_grid,
-                transformation_name=None,
             )
 
     def run(
@@ -94,14 +85,12 @@ class TrainModel:
         Runs training over all estimators (with optional target transformations)
         and performs either grid search or cross-validation.
         """
-        for (
-            estimator,
-            local_param_grid,
-            transformer_name,
-        ) in self.transform_estimator():
-            results = self.fit_estimator(estimator, X_train, X_test, y_train)
+        for evaluation in self.transform_estimator():
+            results = self.fit_estimator(
+                evaluation.estimator, evaluation.param_grid, X_train, X_test, y_train
+            )
             yield TrainResult(
                 runner_result=results,
-                param_grid=local_param_grid,
-                transformation_name=transformer_name,
+                param_grid=evaluation.param_grid,
+                transformation=evaluation.transformation,
             )
