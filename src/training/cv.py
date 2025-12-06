7e7db329a284b02542f34e3aa85ffcdf9f0e6a75
nxfive <kontakt@nxfive.pl>
2025-12-06 16:25:07 +0100

    refactor: Change parameter type from DictConfig to CVConfig dataclass

--- src/training/cv.py
+++ src/training/cv.py
@@ -1,13 +1,14 @@
-from omegaconf import DictConfig
 from sklearn.model_selection import KFold
 
+from src.conf.schema import CVConfig
 
-def get_cv(cfg: DictConfig) -> KFold:
+
+def get_cv(cfg: CVConfig) -> KFold:
     """
     Returns a KFold cross-validator configured based on the values from config.
     """
     return KFold(
-        n_splits=cfg.cv.n_splits,
-        shuffle=cfg.cv.shuffle,
-        random_state=cfg.cv.random_state,
+        n_splits=cfg.n_splits,
+        shuffle=cfg.shuffle,
+        random_state=cfg.random_state,
     )
