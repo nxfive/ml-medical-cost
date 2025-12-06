d0c9984313a38623106f173324171bb3a1fa1c04
nxfive <kontakt@nxfive.pl>
2025-12-06 16:10:40 +0100

    refactor: Add processed_dir attribute to Data constructor

--- src/data/data.py
+++ src/data/data.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pandas as pd
 
 from src.data.split import split_features_target, split_train_test
@@ -6,9 +8,12 @@ from .core import DataFetcher, DataSaver
 
 
 class Data:
-    def __init__(self, data_saver: DataSaver, data_fetcher: DataFetcher):
+    def __init__(
+        self, data_saver: DataSaver, data_fetcher: DataFetcher, processed_dir: Path
+    ):
         self.data_saver = data_saver
         self.data_fetcher = data_fetcher
+        self.processed_dir = processed_dir
 
     def fetch(self) -> pd.DataFrame:
         """
@@ -24,4 +29,6 @@ class Data:
         """
         X, y = split_features_target(df, target_col)
         split_data = split_train_test(X, y)
-        self.data_saver.save_splitted_data(split_data)
+        self.data_saver.save_splitted_data(
+            splits=split_data, processed_dir=self.processed_dir
+        )
