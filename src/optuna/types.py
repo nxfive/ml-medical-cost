1f3113db55635640439aa5726c7f3e5351045c8d
nxfive <kontakt@nxfive.pl>
2025-12-07 12:52:50 +0100

    feat: Add dataclasses for LoadedModelResults and OptunaResult

---
+++ src/optuna/types.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+from sklearn.base import BaseEstimator
+
+import optuna
+from src.models.types import ModelRun
+
+
+@dataclass
+class LoadedModelResults:
+    runs: dict[str, ModelRun]
+
+
+@dataclass
+class OptunaResult:
+    model_class: type[BaseEstimator]
+    best_estimator: BaseEstimator
+    best_params: dict[str, list]
+    transformation: str
+    metrics: dict[str, float]
+    study: optuna.Study
