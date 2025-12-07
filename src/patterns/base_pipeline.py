9581f238a8c7bcf13b6ebdf397ba5c02d1759a6a
nxfive <kontakt@nxfive.pl>
2025-12-07 12:20:15 +0100

    feat: Add TypeVar B to BasePipeline for generic build return type

--- src/patterns/base_pipeline.py
+++ src/patterns/base_pipeline.py
@@ -2,11 +2,12 @@ from abc import ABC, abstractmethod
 from typing import Generic, TypeVar
 
 T = TypeVar("T")
+B = TypeVar("B")
 
 
-class BasePipeline(ABC, Generic[T]):
+class BasePipeline(ABC, Generic[B, T]):
     @abstractmethod
-    def build(self) -> None: ...
+    def build(self) -> B: ...
 
     @abstractmethod
     def run(self) -> T: ...
