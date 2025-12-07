87e6d5eb7f72eaad8a3994b4014e8ff2d84f9719
nxfive <kontakt@nxfive.pl>
2025-12-07 12:59:36 +0100

    refactor: Update metric functions to return dataclass instances

--- src/evaluation/metrics.py
+++ src/evaluation/metrics.py
@@ -5,17 +5,17 @@ from .types import AllMetrics, SplitMetrics, YType
 
 
 def compute_split_metrics(y: YType, y_pred: YType) -> SplitMetrics:
-    return {
-        "r2": r2_score(y, y_pred),
-        "mae": mean_absolute_error(y, y_pred),
-        "rmse": root_mean_squared_error(y, y_pred),
-    }
+    return SplitMetrics(
+        r2=r2_score(y, y_pred),
+        mae=mean_absolute_error(y, y_pred),
+        rmse=root_mean_squared_error(y, y_pred),
+    )
 
 
 def get_metrics(
-    y_train: YType, y_test: YType, y_train_pred: YType, y_test_pred: YType
+    y_train: YType, y_test: YType, train_predictions: YType, test_predictions: YType
 ) -> AllMetrics:
-    return {
-        "train": compute_split_metrics(y_train, y_train_pred),
-        "test": compute_split_metrics(y_test, y_test_pred),
-    }
+    return AllMetrics(
+        train=compute_split_metrics(y_train, train_predictions),
+        test=compute_split_metrics(y_test, test_predictions),
+    )
