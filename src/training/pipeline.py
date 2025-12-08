0d2601d7aa0e4cb20baa930684a0251f7d8680c6
nxfive <kontakt@nxfive.pl>
2025-12-09 00:04:33 +0100

    refactor: Unify TrainingPipeline and BuildResult structure
    Refactored TrainingPipeline to use a new TrainingBuildResult class,
    which includes DataLoader and DataSaver instead of separate readers
    and writers.

--- src/training/pipeline.py
+++ src/training/pipeline.py
@@ -10,12 +10,12 @@ from src.evaluation.metrics import flatten_metrics, get_metrics
 from src.factories.io_factory import IOFactory
 from src.factories.training_factory import TrainingFactory
 from src.io.file_ops import PathManager
-from src.io.types import Readers, Writers
 from src.mlflow.logging import log_model, setup_mlflow
 from src.models.registry import get_model_class_and_short
 from src.models.savers.run_saver import RunSaver
 from src.models.types import ModelLog, ModelResult, ModelRun
 from src.patterns.base_pipeline import BasePipeline
+from src.patterns.types import BuildResult
 
 from .train import TrainModel
 from .types import TrainResult
@@ -23,10 +23,8 @@ from .validation import ModelDiagnostics
 
 
 @dataclass
-class BuildResult:
+class TrainingBuildResult(BuildResult):
     model_class: type[BaseEstimator]
-    writers: Writers
-    readers: Readers
     training: TrainModel
     model_name: str = field(init=False)
 
@@ -34,30 +32,34 @@ class BuildResult:
         self.model_name = self.model_class.__name__
 
 
-class TrainingPipeline(BasePipeline[BuildResult, None]):
-    def __init__(self, training_cfg: TrainingStageConfig):
-        self.training_cfg = training_cfg
+class TrainingPipeline(BasePipeline[TrainingBuildResult, None]):
+    def __init__(self, cfg: TrainingStageConfig):
+        self.cfg = cfg
 
-    def build(self) -> BuildResult:
+    def build(self) -> TrainingBuildResult:
         """
         Constructs and returns the components required for training pipeline.
         """
-        PathManager.ensure_dir(self.training_cfg.trainig_dir.output_dir)
-        model_class, _ = get_model_class_and_short(self.training_cfg.model.name)
+        PathManager.ensure_dir(self.cfg.training_dir.output_dir)
+        model_class, _ = get_model_class_and_short(self.cfg.model.name)
         writers = IOFactory.create_writers()
         readers = IOFactory.create_readers()
-        training = TrainingFactory.create(model_class, self.training_cfg)
-        return BuildResult(
-            model_class=model_class, writers=writers, readers=readers, training=training
+        data_loader = DataLoader(readers)
+        data_saver = DataSaver(writers)
+        training = TrainingFactory.create(model_class, self.cfg)
+        return TrainingBuildResult(
+            model_class=model_class,
+            saver=data_saver,
+            loader=data_loader,
+            training=training,
         )
 
-    def load_data(self, readers: Readers) -> SplitData:
+    def load_data(self, data_loader: DataLoader) -> SplitData:
         """
         Loads preprocessed training and test data using the provided readers.
         """
-        data_loader = DataLoader(readers)
         return data_loader.load_splitted_data(
-            processed_dir=self.training_cfg.data_dir.processed_dir
+            processed_dir=self.cfg.data_dir.processed_dir
         )
 
     @staticmethod
@@ -115,10 +117,10 @@ class TrainingPipeline(BasePipeline[BuildResult, None]):
         """
         setup_mlflow()
         builder = self.build()
-        split_data = self.load_data(builder.readers)
-        data_saver = DataSaver(builder.writers)
+        split_data = self.load_data(builder.loader)
+
         run_saver = RunSaver(
-            trainig_dir=self.training_cfg.trainig_dir, data_saver=data_saver
+            trainig_dir=self.cfg.training_dir, data_saver=builder.saver
         )
 
         for train_result in builder.training.run(
@@ -133,7 +135,6 @@ class TrainingPipeline(BasePipeline[BuildResult, None]):
                 train_predictions=train_result.runner_result.train_predictions,
                 test_predictions=train_result.runner_result.test_predictions,
             )
-
             diagnostics = ModelDiagnostics(
                 folds_scores=train_result.runner_result.folds_scores
             )
