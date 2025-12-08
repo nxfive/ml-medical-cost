82dd8c1f3daeed81154b1cc42136eda35c79f409
nxfive <kontakt@nxfive.pl>
2025-12-08 20:31:09 +0100

    refactor: Move param_grid and pipeline to TrainModel, add docstring

--- src/factories/training_factory.py
+++ src/factories/training_factory.py
@@ -1,42 +1,46 @@
 from sklearn.base import BaseEstimator
 
 from src.conf.schema import TrainingStageConfig
-from src.models.pipeline import create_model_pipeline
+from src.models.models import create_model_pipeline
+from src.params.grid import ParamGrid
+from src.training.cv import get_cv
 from src.training.train import TrainModel
 from src.training.tuning import (CrossValidationRunner, GridSearchRunner,
                                  TargetTransformer)
-from src.utils.cv import get_cv
-from src.utils.grid import ParamGrid
 
 
 class TrainingFactory:
     @staticmethod
-    def create(model_class: type[BaseEstimator], training_cfg: TrainingStageConfig):
-        cv = get_cv(training_cfg.cv)
+    def create(
+        model_class: type[BaseEstimator], cfg: TrainingStageConfig
+    ) -> TrainModel:
+        """
+        Creates and configures a TrainModel instance with the given model class and config.
+        """
+        cv = get_cv(cfg.cv)
 
         pipeline = create_model_pipeline(
-            training_cfg.model.preprocess_num_features,
-            training_cfg.features,
+            preprocess_num_features=cfg.model.preprocess_num_features,
+            features_cfg=cfg.features,
             model=model_class,
         )
+        param_grid = ParamGrid.create(cfg.model.params)
 
-        param_grid = ParamGrid.create(training_cfg.model.params)
-
-        grid_runner = GridSearchRunner(cv=cv, pipeline=pipeline, param_grid=param_grid)
-        cross_runner = CrossValidationRunner(cv=cv, pipeline=pipeline)
+        grid_runner = GridSearchRunner(cv=cv)
+        cross_runner = CrossValidationRunner(cv=cv)
 
         target_transformer = TargetTransformer(
-            pipeline=pipeline,
-            param_grid=param_grid,
-            training_cfg_transform=training_cfg.transformers,
+            cfg_transform=cfg.transformers,
         )
 
         return TrainModel(
             model=model_class,
-            cfg_model=training_cfg.model,
-            cfg_features=training_cfg.features,
-            cfg_cv=training_cfg.cv,
-            cfg_transform=training_cfg.transformers,
+            cfg_model=cfg.model,
+            cfg_features=cfg.features,
+            cfg_cv=cfg.cv,
+            cfg_transform=cfg.transformers,
+            param_grid=param_grid,
+            pipeline=pipeline,
             grid_runner=grid_runner,
             cross_runner=cross_runner,
             target_transformer=target_transformer,
