8674f8f2b677628ecaaa4006dbef3d2bc306b274
nxfive <kontakt@nxfive.pl>
2025-12-11 22:00:29 +0100

    refactor: Move dataclasses related to runners/transformers
    Moved to tuning/types.py

---
+++ src/tuning/types.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+
+import numpy as np
+from sklearn.base import BaseEstimator
+
+
+@dataclass
+class PredictionResult:
+    trained: BaseEstimator
+    train_predictions: np.ndarray
+    test_predictions: np.ndarray
+
+
+@dataclass
+class RunnerResult(PredictionResult):
+    folds_scores: list[np.float64]
+    folds_scores_mean: np.float64
+
+
+@dataclass
+class EvaluationResult:
+    estimator: BaseEstimator
+    param_grid: dict[str, list]
+    transformation: str = "none"
