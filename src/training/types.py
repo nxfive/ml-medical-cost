7ff739a46efcbc0f56f24236ad70a8bf37717331
nxfive <kontakt@nxfive.pl>
2025-12-12 00:01:52 +0100

    refactor: Move dataclasses and TransformersDict to tuning/types.py

--- src/training/types.py
+++ src/training/types.py
@@ -1,28 +1,6 @@
 from dataclasses import dataclass
-from typing import TypedDict
 
-import numpy as np
-from sklearn.base import BaseEstimator
-
-
-@dataclass
-class PredictionResult:
-    trained: BaseEstimator
-    train_predictions: np.ndarray
-    test_predictions: np.ndarray
-
-
-@dataclass
-class RunnerResult(PredictionResult):
-    folds_scores: list[np.float64]
-    folds_scores_mean: np.float64
-
-
-@dataclass
-class EvaluationResult:
-    estimator: BaseEstimator
-    param_grid: dict[str, list]
-    transformation: str = "none"
+from src.tuning.types import RunnerResult
 
 
 @dataclass
@@ -30,9 +8,3 @@ class TrainResult:
     runner_result: RunnerResult
     param_grid: dict[str, list]
     transformation: str | None
-
-
-class TransformersDict(TypedDict):
-    log: BaseEstimator
-    quantile: BaseEstimator
-    none: None
