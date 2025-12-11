08b95f92a4dcf0c6a5849b4198fd468faaa0ca46
nxfive <kontakt@nxfive.pl>
2025-12-11 22:09:27 +0100

    refactor: Improve BaseRunner structure and helper methods
    - make run() abstract with empty body
    - add static method fit_estimator for estimator fitting
    - convert _collect_results to instance method using self

--- src/tuning/runners/base_runner.py
+++ src/tuning/runners/base_runner.py
@@ -10,10 +10,7 @@ from src.training.types import RunnerResult
 
 class BaseRunner(ABC):
     @abstractmethod
-    def run(self, *args, **kwargs) -> RunnerResult:
-        """
-        Runs the training/evaluation. Must be implemented in subclasses.
-        """
+    def run(self, *args, **kwargs) -> RunnerResult: ...
 
     @staticmethod
     def make_predictions(estimator: BaseEstimator, X: pd.DataFrame) -> np.ndarray:
@@ -21,11 +18,27 @@ class BaseRunner(ABC):
         Generates predictions using the trained estimator.
         """
         return estimator.predict(X)
-
+    
     @staticmethod
+    def fit_estimator(
+        estimator: BaseEstimator,
+        X_train: pd.DataFrame,
+        y_train: pd.Series,
+        return_best: bool = False
+    ) -> BaseEstimator:
+        """
+        Fits the estimator. For search objects like GridSearchCV or OptunaSearchCV,
+        `return_best=True` returns the best_estimator_.
+        """
+        estimator.fit(X_train, y_train)
+        if return_best and hasattr(estimator, "best_estimator_"):
+            return estimator.best_estimator_
+        return estimator
+
     def _collect_results(
+        self,
         trained: BaseEstimator,
-        folds_scores: list[float],
+        folds_scores: list[np.float64],
         X_train: pd.DataFrame,
         X_test: pd.DataFrame,
     ) -> RunnerResult:
@@ -33,8 +46,8 @@ class BaseRunner(ABC):
         Creates RunnerResult from trained estimator and CV scores.
         """
         folds_scores_mean = compute_scores_mean(folds_scores)
-        train_predictions = BaseRunner.make_predictions(trained, X_train)
-        test_predictions = BaseRunner.make_predictions(trained, X_test)
+        train_predictions = self.make_predictions(trained, X_train)
+        test_predictions = self.make_predictions(trained, X_test)
         return RunnerResult(
             trained=trained,
             folds_scores=folds_scores,
