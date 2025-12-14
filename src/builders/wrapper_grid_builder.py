cd02e747a24eebc81cd42c6a317cbc6c7beddc46
nxfive <kontakt@nxfive.pl>
2025-12-14 06:09:34 +0100

    feat: Add WrapperGridBuilder for validated wrapper parameter grid

---
+++ src/builders/wrapper_grid_builder.py
@@ -0,0 +1,19 @@
+from src.params.grid import ParamGridPrefixer
+from src.params.validator import ParamValidator
+
+
+class WrapperGridBuilder:
+    @staticmethod
+    def build(
+        param_grid: dict[str, list], transformer_params: dict[str, list] | None
+    ) -> dict[str, list]:
+        """
+        Builds a validated parameter grid for wrapper estimators, applying
+        namespaces for model and transformer parameters.
+        """
+        if transformer_params:
+            ParamValidator.validate_grid(transformer_params)
+
+        return ParamGridPrefixer().prepare_wrapper_grid(
+            param_grid=param_grid, transformer_params=transformer_params
+        )
