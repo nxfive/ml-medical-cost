47c9255ef4ae65d0d317596322a01a10bb4cd182
nxfive <kontakt@nxfive.pl>
2025-12-14 09:37:52 +0100

    feat: Add OptunaExperimentBuilder to build ExperimentSetup

---
+++ src/builders/optuna_experiment_builder.py
@@ -0,0 +1,39 @@
+import optuna
+from src.optuna.types import ExperimentSetup, OptunaExperimentConfig
+
+from .optuna_grid_distribution_builder import OptunaGridDistributionBuilder
+from .optuna_trial_grid_builder import OptunaTrialGridBuilder
+from .pipeline_builder import PipelineBuilder
+
+
+class OptunaExperimentBuilder:
+    @staticmethod
+    def build(
+        cfg: OptunaExperimentConfig, trial: optuna.Trial | None = None
+    ) -> ExperimentSetup:
+        """
+        Builds an ExperimentSetup, including the pipeline and parameter space.
+
+        If a trial is provided, samples parameters for that trial.
+        Otherwise, builds the full Optuna search space.
+        """
+        params = (
+            OptunaTrialGridBuilder.build(
+                trial=trial,
+                optuna_params=cfg.optuna_model_config.params,
+                model_params=cfg.model.params,
+                transformers=cfg.transformers.to_dict(),
+            )
+            if trial is not None
+            else OptunaGridDistributionBuilder.build(
+                optuna_params=cfg.optuna_model_config.params,
+                model_params=cfg.model.params,
+            )
+        )
+
+        pipeline = PipelineBuilder.build(
+            model_cfg=cfg.model,
+            features_cfg=cfg.features,
+            transformation=params.get("transformation", "none"),
+        )
+        return ExperimentSetup(pipeline, params)
