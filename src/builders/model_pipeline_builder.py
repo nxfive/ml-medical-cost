880f55d81ce8c42b0612792eac0197f2a119e55e
nxfive <kontakt@nxfive.pl>
2025-12-14 05:56:57 +0100

    refactor: Remove random_state injection from ModelPipelineBuilder

--- src/builders/model_pipeline_builder.py
+++ src/builders/model_pipeline_builder.py
@@ -1,6 +1,3 @@
-import inspect
-from typing import Any
-
 from sklearn.base import BaseEstimator
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
@@ -11,17 +8,10 @@ class ModelPipelineBuilder:
     def build(
         preprocessor: ColumnTransformer,
         model: type[BaseEstimator],
-        params: dict[str, Any] | None = None,
     ) -> Pipeline:
         """
         Builds a pipeline consisting of a preprocessor and a model.
         """
-        params = params or {}
-
-        signature = inspect.signature(model.__init__)
-        if "random_state" in signature.parameters:
-            params.setdefault("random_state", 42)
-
         return Pipeline(
             [
                 ("preprocessor", preprocessor),
