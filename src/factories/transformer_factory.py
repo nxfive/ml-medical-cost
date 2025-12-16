5954f50941666694f5e33e4a05f70d34f4ce8a13
nxfive <kontakt@nxfive.pl>
2025-12-16 17:09:02 +0100

    feat: Add TargetTransformerFactory for creating target transformers

---
+++ src/factories/transformer_factory.py
@@ -0,0 +1,19 @@
+from typing import Any
+
+from sklearn.base import TransformerMixin
+
+from src.tuning.transformers.registry import TRANSFORMERS
+
+
+class TargetTransformerFactory:
+    @staticmethod
+    def create(transformation: str) -> TransformerMixin:
+        """
+        Creates an instance of a target transformer with given parameters.
+
+        Only includes technical parameters that do not affect model results.
+        """
+        spec = TRANSFORMERS.get(transformation)
+        if spec is None:
+            raise ValueError(f"Transformer '{transformation}' not found")
+        return spec.spec_class(**spec.spec_params)
