1e0ea94d744246fc6f04b40e30e3ac61433861db
nxfive <kontakt@nxfive.pl>
2025-12-17 14:36:42 +0100

    refactor: Move _compute_metrics to BasePipeline for DRY

--- src/patterns/base_pipeline.py
+++ src/patterns/base_pipeline.py
@@ -1,8 +1,12 @@
 from abc import ABC, abstractmethod
 from typing import Generic, TypeVar
 
-T = TypeVar("T")
+from src.evaluation.metrics import get_metrics
+from src.evaluation.types import AllMetrics
+from src.patterns.types import PredictionSet
+
 B = TypeVar("B")
+T = TypeVar("T")
 
 
 class BasePipeline(ABC, Generic[B, T]):
@@ -11,3 +15,15 @@ class BasePipeline(ABC, Generic[B, T]):
 
     @abstractmethod
     def run(self) -> T: ...
+
+    @staticmethod
+    def _compute_metrics(pred_set: PredictionSet) -> AllMetrics:
+        """
+        Computes training and test metrics from a PredictionSet.
+        """
+        return get_metrics(
+            y_train=pred_set.y_train,
+            y_test=pred_set.y_test,
+            train_predictions=pred_set.train_predictions,
+            test_predictions=pred_set.test_predictions,
+        )
