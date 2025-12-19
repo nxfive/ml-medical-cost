32908c06b819711860c26ec897772f8f1a7e95bf
nxfive <kontakt@nxfive.pl>
2025-12-20 00:36:14 +0100

    feat: Add IdentityTransformer as a none Transformer

---
+++ src/tuning/transformers/identity.py
@@ -0,0 +1,13 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class IdentityTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self): ...
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return X
+
+    def inverse_transform(self, X):
+        return X
