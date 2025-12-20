06bb1b10e03f0820afbea0d71a165abcac6fe62d
nxfive <kontakt@nxfive.pl>
2025-12-20 19:42:43 +0100

    refactor: Replace types files with DTOs, containers, and serializers

--- src/params/types.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class Prefixes(str, Enum):
-    PIPELINE_MODEL = "model__"
-    WRAPPER_REGRESSOR = "regressor__"
-    WRAPPER_TRANSFORMER = "transformer__"
--- src/patterns/types.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-from src.data.core import DataLoader, DataSaver
-
-
-@dataclass
-class BuildResult:
-    loader: DataLoader
-    saver: DataSaver
