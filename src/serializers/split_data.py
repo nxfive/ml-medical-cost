bef3c64f36215458597bc163346e0f7afe5945de
nxfive <kontakt@nxfive.pl>
2025-12-21 22:41:06 +0100

    fix: Remove implicit state from SplitDataSerializer

--- src/serializers/split_data.py
+++ src/serializers/split_data.py
@@ -5,15 +5,16 @@ from src.containers.types import SplitDataDict
 
 
 class SplitDataSerializer:
-    def to_dict(self) -> SplitDataDict:
+    @staticmethod
+    def to_dict(split_data: SplitData) -> SplitDataDict:
         def to_df(s: pd.Series) -> pd.DataFrame:
             return s.to_frame() if isinstance(s, pd.Series) else s
 
         return {
-            "X_train": self.X_train,
-            "X_test": self.X_test,
-            "y_train": to_df(self.y_train),
-            "y_test": to_df(self.y_test),
+            "X_train": split_data.X_train,
+            "X_test": split_data.X_test,
+            "y_train": to_df(split_data.y_train),
+            "y_test": to_df(split_data.y_test),
         }
 
     @staticmethod
