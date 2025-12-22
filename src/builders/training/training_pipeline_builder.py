ae9030d900f706342e6af66d40a2794a6ad5498d
nxfive <kontakt@nxfive.pl>
2025-12-22 23:45:31 +0100

    refactor: Make TrainingPipelineBuilder inherit from BasePipelineBuilder
    - Inherited from BasePipelineBuilder with generics for stage config, saver, and build result
    - Replaced _build_run_saver with base class method _build_saver

--- src/builders/training/training_pipeline_builder.py
+++ src/builders/training/training_pipeline_builder.py
@@ -1,41 +1,24 @@
 from sklearn.base import BaseEstimator
 
+from src.builders.base.base_pipeline_builder import BasePipelineBuilder
 from src.conf.schema import TrainingStageConfig
 from src.containers.builder import TrainingBuildResult
-from src.data.core import DataLoader, DataSaver
-from src.factories.io_factory import IOFactory
-from src.factories.model_factory import ModelFactory
+from src.data.core import DataSaver
 from src.io.file_ops import PathManager
 from src.models.savers.run_saver import RunSaver
-from src.models.spec import ModelSpec
 from src.training.train import TrainModel
 
 from .training_builder import TrainingBuilder
 
 
-class TrainingPipelineBuilder:
-    def __init__(self, cfg: TrainingStageConfig):
-        self.cfg = cfg
-
-    def _build_data_io(self) -> tuple[DataLoader, DataSaver]:
-        """
-        Builds the DataLoader and DataSaver using IOFactory readers/writers.
-        """
-        readers = IOFactory.create_readers()
-        writers = IOFactory.create_writers()
-        return DataLoader(readers), DataSaver(writers)
-
-    def _build_run_saver(self, data_saver: DataSaver) -> RunSaver:
+class TrainingPipelineBuilder(
+    BasePipelineBuilder[TrainingStageConfig, RunSaver, TrainingBuildResult]
+):
+    def _build_saver(self, data_saver: DataSaver) -> RunSaver:
         """
         Builds a RunSaver to save training runs.
         """
-        return RunSaver(trainig_dir=self.cfg.training_dir, data_saver=data_saver)
-
-    def _build_model_spec(self) -> ModelSpec:
-        """
-        Builds the model specification from the ModelFactory.
-        """
-        return ModelFactory.get_spec(self.cfg.model.name)
+        return RunSaver(training_dir=self.cfg.training_dir, data_saver=data_saver)
 
     def _build_training(self, model_class: type[BaseEstimator]) -> TrainModel:
         """
@@ -54,7 +37,7 @@ class TrainingPipelineBuilder:
         """
         PathManager.ensure_dir(self.cfg.training_dir.output_dir)
         data_loader, data_saver = self._build_data_io()
-        run_saver = self._build_run_saver(data_saver)
+        run_saver = self._build_saver(data_saver)
         model_spec = self._build_model_spec()
         training = self._build_training(model_spec.model_class)
 
