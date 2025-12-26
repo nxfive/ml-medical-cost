50b4f7c36537c07a72746152a0df79bea9a693d9
nxfive <kontakt@nxfive.pl>
2025-12-26 23:43:09 +0100

    chore: Add info logs and timing to the data pipeline

--- src/data/pipeline.py
+++ src/data/pipeline.py
@@ -1,7 +1,10 @@
+import time
+
 from src.conf.schema import DataStageConfig
 from src.factories.data_factory import DataFactory
 from src.features.core import convert_features_type
 from src.io.file_ops import PathManager
+from src.logger.setup import logger
 from src.patterns.base_pipeline import BasePipeline
 
 
@@ -21,7 +24,20 @@ class DataPipeline(BasePipeline[None, None]):
         """
         Executes a full data pipeline.
         """
+        logger.info("Running data stage")
+        start_data = time.perf_counter()
+
+        logger.info("Initializing data pipeline environment")
         self.build()
+
+        logger.info("Fetching dataset")
         df = self.data.fetch()
+
+        logger.info("Converting features to numeric types [int/float]")
         df = convert_features_type(df)
+
+        logger.info("Preparing train/test splits")
         self.data.split(df)
+
+        end_data = time.perf_counter()
+        logger.info(f"Data stage completed in {end_data - start_data:.2f}s")
